fd5121279a9723cefefbd6ebc95b9a193add888e
django <maja0270558@gmail.com>
2023-10-23 14:18:05 +0800

    init commit

---
+++ Gebble/GebbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GebbleApp.swift
+//  Gebble
+//
+//  Created by DjangoLin on 2023/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GebbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
