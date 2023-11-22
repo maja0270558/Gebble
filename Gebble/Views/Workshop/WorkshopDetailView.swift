7397a00a564fd0ea048eab1edce6ff481e70635a
django <maja0270558@gmail.com>
2023-11-22 18:47:38 +0800

    Refactor workshop detail

--- Gebble/Views/Workshop/WorkshopDetailView.swift
+++ Gebble/Views/Workshop/WorkshopDetailView.swift
@@ -20,7 +20,6 @@ struct WorkshopDetailFeature: Reducer {
     }
 
     enum Action: Equatable {
-        case closeButtonClick
         case attendWorkshoptaped
         case load
         case detailResponse(TaskResult<WorkshopDetail>)
@@ -45,8 +44,6 @@ struct WorkshopDetailFeature: Reducer {
             }
             return .none
 
-        case .closeButtonClick:
-            return .none
         case .attendWorkshoptaped:
             shareStateClient.update(\.globalStore, action: .loginViewPresent(true))
             return .none
@@ -56,15 +53,7 @@ struct WorkshopDetailFeature: Reducer {
 
 struct WorkshopDetailView: View {
     let store: StoreOf<WorkshopDetailFeature>
-
-    private let minHeight = 150.0
-    private let maxHeight = UIScreen.main.bounds.height / 2
-
-    @State var progress: CGFloat = 0
     @State var isLoading = false
-
-    @Namespace var animation
-    @Environment(\.dismiss) var dismiss
 
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
@@ -73,82 +62,16 @@ struct WorkshopDetailView: View {
 
                 ScrollView {
                     LazyVStack(alignment: .leading, spacing: 0, pinnedViews: [.sectionHeaders]) {
-                        
-                        GebbleImage(url: viewStore.state.detail?.poster)
-                            .scaledToFill()
-                            .background(.white)
-                            .cornerRadius(20, corners: [.bottomLeft, .bottomRight])
-                            .shadow(radius: 5)
-
-
-                        Section {
-                            Group {
-                                HStack {
-                                    Image(systemName: "person")
-                                    Text("\(viewStore.state.detail?.name1 ?? "")")
-                                }
-
-                                HStack {
-                                    HStack {
-                                        Image(systemName: "calendar")
-                                        Text("\(viewStore.state.detail?.startDate ?? "") \(viewStore.state.detail?.datetime ?? "")")
-                                    }
-
-                                    HStack {
-                                        Image(systemName: "mappin.and.ellipse")
-                                        Text("\(viewStore.state.detail?.country ?? "") \(viewStore.state.detail?.venue ?? "")")
-                                    }
-                                }
-                            }.padding(.horizontal)
-                          
-                        } header: {
-                            HStack {
-                                Text("\(viewStore.state.detail?.title ?? "")")
-                                    .font(.title.bold())
-                                    Spacer()
-                            }
-                            .padding()
-                            .background(Color.base)
-                        }
-
-                        Section {
-                            HStack {
-                                Text("\(viewStore.state.detail?.content ?? "")")
-                                    .padding()
-                                    .placeholder(isLoading) {
-                                        Text.textPlaceholder()
-                                    }
-                            }
-                            
-                        } header: {
-                            HStack {
-                                Text("About the workshop")
-                                    .font(.title.bold())
-                                    Spacer()
-                            }
-                            .padding()
-                            .background(Color.base)
-                            
-                        }
+                        topImage
+                        sectionTitle
+                        sectiionContent
                         Spacer()
                     }
                 }
                 .placeholder(isLoading)
-                
+
                 .safeAreaInset(edge: .bottom, content: {
-                    Button(action: {
-                        viewStore.send(.attendWorkshoptaped)
-                    }, label: {
-                        Text("Register")
-                            .bold()
-                            .frame(height: 65)
-                            .frame(maxWidth: .infinity)
-                            .background(.brown)
-                            .foregroundColor(.white)
-                            .cornerRadius(12, corners: .allCorners)
-                            .padding()
-                    })
-                    
+                    bottomRegisterButton
                 })
             }
             .navigationTitle("\(viewStore.detail?.name1 ?? "")")
@@ -162,169 +85,103 @@ struct WorkshopDetailView: View {
         }
     }
 
-    private var header: some View {
-        WithViewStore(self.store, observe: { $0 }) { viewStore in
-            ZStack {
-                GebbleImage(url: viewStore.state.detail?.poster)
-                    .aspectRatio(contentMode: .fill)
-                    .clipped()
-                    .cornerRadius(20)
-                    .frame(minHeight: maxHeight)
-            }
-            .opacity(1 - progress)
-        }
-    }
-
-    private var topButtons: some View {
-        WithViewStore(self.store, observe: { $0 }) { viewStore in
-
-            VStack {
-                HStack {
-                    Button("", action: { viewStore.send(.closeButtonClick) })
-                        .buttonStyle(CircleButtonStyle(imageName: "arrow.backward"))
-                        .padding(.leading, 17)
-                    Spacer()
-                    Button("", action: { print("Info") })
-                        .buttonStyle(CircleButtonStyle(imageName: "ellipsis"))
-                        .padding(.trailing, 17)
-                }
-
-                Spacer()
-            }
-        }
-    }
-
-    private var navgationBar: some View {
+    private var topImage: some View {
         WithViewStore(self.store, observe: \.detail) { viewStore in
-
-            VStack(spacing: 0) {
-                HStack {
-                    Text("\(viewStore.state?.name1 ?? "")")
-                        .frame(height: 40)
-                }
-                .frame(maxWidth: .infinity, alignment: .center)
-                .background(Color.base)
-
-                Spacer()
-            }
-            .opacity(max(0, min(1, (progress - 0.75) * 4.0)))
+            GebbleImage(url: viewStore.state?.poster)
+                .scaledToFill()
+                .background(.white)
+                .cornerRadius(20, corners: [.bottomLeft, .bottomRight])
+                .shadow(radius: 5)
         }
     }
-//
-//
-//    private var video: some View {
-//        WithViewStore(self.store, observe: { $0 }) { viewStore in
-//            VStack(alignment: .center, spacing: 20) {
-//                if let vidArr = viewStore.bios?.videos, vidArr.count > 0 {
-//                    ForEach(vidArr, id: \.id) { vid in
-//                        youtubeBuilder(url: vid.url)
-//                    }
-//                } else {
-//                    VStack {
-//                        HStack {
-//                            Image(systemName: "figure.yoga")
-//                            Text("Stay tune! This artist not upload any video yet")
-//                        }
-//                    }
-//                    .foregroundStyle(.gray)
-//                }
-//            }
-//        }
-//    }
-//
-//    @Environment(\.openURL) var openURL
-//
-//    private var about: some View {
-//        WithViewStore(self.store, observe: { $0 }) { viewStore in
-//
-//            VStack(alignment: .leading, spacing: 20) {
-//                if let contacts = viewStore.bios?.contacts, contacts.count > 0 {
-//                    HStack {
-//                        ForEach(contacts, id: \.id) { contact in
-//                            if let url = URL(string: contact.url!), UIApplication.shared.canOpenURL(url) {
-//                                Image("\(contact.type.imageName)")
-//                                    .resizable()
-//                                    .frame(width: 40, height: 40)
-//                                    .onTapGesture {
-//                                        openURL(url)
-//                                    }
-//                            }
-//                        }
-//                    }
-//                }
-//
-//                if let crew = viewStore.bios?.crew, !crew.isEmpty {
-//                    Text("\(crew)").font(.title2)
-//                }
-//
-//                if let quote = viewStore.bios?.quote,
-//                   let user = viewStore.state.portfolios?.artistName
-//                {
-//                    Divider()
-//                    HStack {
-//                        Text("\"\(quote)\" - \(user)").fontWeight(.thin).font(.callout.bold().italic())
-//                        if let coutry = viewStore.portfolios?.country {
-//                            let flag = convertStringToFlag(coutry)
-//                            Text("\(flag)")
-//                        }
-//                    }
-//                }
-//
-//                if let intro = viewStore.portfolios?.introduction {
-//                    Divider()
-//                    Text("\(intro)").font(.body)
-//                }
-//
-//                Spacer()
-//            }
-//        }
-//    }
-//
-//    @ViewBuilder
-//    func youtubeBuilder(url: String) -> some View {
-//        YouTubePlayerView(
-//            .init(stringLiteral: "\(url)")
-//        ) {
-//            state in
-//            switch state {
-//            case .idle:
-//                ProgressView()
-//            case .ready:
-//                EmptyView()
-//            case .error:
-//                Text(verbatim: "YouTube player couldn't be loaded")
-//            }
-//        }
-//        .background(Color.gray.opacity(0.3))
-//        .frame(minHeight: 240)
-//        .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
-//    }
-//
-//    private var jouney: some View {
-//        WithViewStore(self.store, observe: { $0 }) { _ in
-//            VStack {
-//                HStack {
-//                    Image(systemName: "lock.fill")
-//                    Text("Access is not allowed.")
-//                }
-//                Text("To view their journey, you need to give a shoutout or get one and wait for approval.")
-//            }
-//            .foregroundStyle(.gray)
-//        }
-//    }
-//
-//    private var e1t1: some View {
-//        WithViewStore(self.store, observe: { $0 }) { _ in
-//            VStack {
-//                HStack {
-//                    Image(systemName: "lock.fill")
-//                    Text("Access is not allowed.")
-//                }
-//                Text("To view their each 1 teach 1, you need to give a shoutout or get one and wait for approval.")
-//            }
-//            .foregroundStyle(.gray)
-//        }
-//    }
+
+    private var sectionTitle: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            
+            Section {
+                Group {
+                    HStack {
+                        Image(systemName: "person")
+                        Text("\(viewStore.state.detail?.name1 ?? "")")
+                    }
+
+                    HStack {
+                        HStack {
+                            Image(systemName: "calendar")
+                            Text("\(viewStore.state.detail?.startDate ?? "") \(viewStore.state.detail?.datetime ?? "")")
+                        }
+                        Spacer()
+                        HStack {
+                            Image(systemName: "mappin.and.ellipse")
+                            Text("\(viewStore.state.detail?.country ?? "") \(viewStore.state.detail?.venue ?? "")")
+                        }
+                    }
+                }
+                .padding(.horizontal)
+            } header: {
+                HStack {
+                    Text("\(viewStore.state.detail?.title ?? "")")
+                        .font(.title.bold())
+                    Spacer()
+                }
+                .padding()
+                .background(Color.base)
+            }
+
+            
+        }
+    }
+
+    private var sectiionContent: some View {
+        WithViewStore(self.store, observe: \.detail) { viewStore in
+            Section {
+                HStack {
+                    if let content = viewStore.state?.content {
+                        if content.isEmpty {
+                            VStack(alignment: .center) {
+                                Text("Creator left a blank space behind 😇")
+                                    .foregroundStyle(.gray)
+                            }
+                           
+                        } else {
+                            Text("\(content)")
+                        }
+                    }
+                }.padding()
+                    .placeholder(isLoading) {
+                        Text.textPlaceholder()
+                    }
+
+            } header: {
+                HStack {
+                    Text("About the workshop")
+                        .font(.title.bold())
+                    Spacer()
+                }
+                .padding()
+                .background(Color.base)
+            }
+        }
+    }
+    
+    private var bottomRegisterButton:  some View {
+        WithViewStore(self.store, observe: \.detail) { viewStore in
+            Button(action: {
+                viewStore.send(.attendWorkshoptaped)
+            }, label: {
+                Text("Register")
+                    .bold()
+                    .frame(height: 65)
+                    .frame(maxWidth: .infinity)
+                    .background(.brown)
+                    .foregroundColor(.white)
+                    .cornerRadius(12, corners: .allCorners)
+                    .padding()
+            })
+        }
+
+    }
+
 }
 
 #Preview {
